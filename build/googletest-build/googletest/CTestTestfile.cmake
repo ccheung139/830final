ea66dfee0f82436904a2036f474ce1d26705ac08
David0Li <46199278+David0Li@users.noreply.github.com>
2021-05-14 01:34:45 -0400

    Delete build directory

--- build/googletest-build/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/christophercheung/Desktop/6.830/830final/build/googletest-src/googletest
-# Build directory: /Users/christophercheung/Desktop/6.830/830final/build/googletest-build/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/test/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/christophercheung/Desktop/6.830/830final/test
-# Build directory: /Users/christophercheung/Desktop/6.830/830final/build/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest-build")
