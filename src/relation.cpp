ed5622fbdd5e6ce27e10d597bdaa25ae32f30333
Christopher Cheung <christophercheung@Christophers-MacBook-Pro-3.local>
2021-05-14 01:28:01 -0400

    adding some random for loops lol

--- src/relation.cpp
+++ src/relation.cpp
@@ -7,24 +7,29 @@
 #include <sys/stat.h>
 
 // Stores a relation into a binary file
-void Relation::storeRelation(const std::string &file_name) {
+void Relation::storeRelation(const std::string &file_name)
+{
   std::ofstream out_file;
   out_file.open(file_name, std::ios::out | std::ios::binary);
-  out_file.write((char *) &size_, sizeof(size_));
+  out_file.write((char *)&size_, sizeof(size_));
   auto numColumns = columns_.size();
-  out_file.write((char *) &numColumns, sizeof(size_t));
-  for (auto c : columns_) {
-    out_file.write((char *) c, size_ * sizeof(uint64_t));
+  out_file.write((char *)&numColumns, sizeof(size_t));
+  for (auto c : columns_)
+  {
+    out_file.write((char *)c, size_ * sizeof(uint64_t));
   }
   out_file.close();
 }
 
 // Stores a relation into a file (csv), e.g., for loading/testing it with a DBMS
-void Relation::storeRelationCSV(const std::string &file_name) {
+void Relation::storeRelationCSV(const std::string &file_name)
+{
   std::ofstream out_file;
   out_file.open(file_name + ".tbl", std::ios::out);
-  for (uint64_t i = 0; i < size_; ++i) {
-    for (auto &c : columns_) {
+  for (uint64_t i = 0; i < size_; ++i)
+  {
+    for (auto &c : columns_)
+    {
       out_file << c[i] << '|';
     }
     out_file << "\n";
@@ -32,12 +37,14 @@ void Relation::storeRelationCSV(const std::string &file_name) {
 }
 
 // Dump SQL: Create and load table (PostgreSQL)
-void Relation::dumpSQL(const std::string &file_name, unsigned relation_id) {
+void Relation::dumpSQL(const std::string &file_name, unsigned relation_id)
+{
   std::ofstream out_file;
   out_file.open(file_name + ".sql", std::ios::out);
   // Create table statement
   out_file << "CREATE TABLE r" << relation_id << " (";
-  for (unsigned cId = 0; cId < columns_.size(); ++cId) {
+  for (unsigned cId = 0; cId < columns_.size(); ++cId)
+  {
     out_file << "c" << cId << " bigint"
              << (cId < columns_.size() - 1 ? "," : "");
   }
@@ -47,15 +54,19 @@ void Relation::dumpSQL(const std::string &file_name, unsigned relation_id) {
            << ".tbl' delimiter '|';\n";
 }
 
-void Relation::loadRelation(const char *file_name) {
+void Relation::loadRelation(const char *file_name)
+{
   int fd = open(file_name, O_RDONLY);
-  if (fd == -1) {
+  if (fd == -1)
+  {
     std::cerr << "cannot open " << file_name << std::endl;
     throw;
   }
 
   // Obtain file size_
-  struct stat sb{};
+  struct stat sb
+  {
+  };
   if (fstat(fd, &sb) == -1)
     std::cerr << "fstat\n";
 
@@ -67,13 +78,15 @@ void Relation::loadRelation(const char *file_name) {
                                         MAP_PRIVATE,
                                         fd,
                                         0u));
-  if (addr == MAP_FAILED) {
+  if (addr == MAP_FAILED)
+  {
     std::cerr << "cannot mmap " << file_name << " of length " << length
               << std::endl;
     throw;
   }
 
-  if (length < 16) {
+  if (length < 16)
+  {
     std::cerr << "relation_ file " << file_name
               << " does not contain a valid header"
               << std::endl;
@@ -84,22 +97,37 @@ void Relation::loadRelation(const char *file_name) {
   addr += sizeof(size_);
   auto numColumns = *reinterpret_cast<size_t *>(addr);
   addr += sizeof(size_t);
-  for (unsigned i = 0; i < numColumns; ++i) {
+
+  for (unsigned i = 0; i < numColumns; ++i)
+  {
     this->columns_.push_back(reinterpret_cast<uint64_t *>(addr));
     addr += size_ * sizeof(uint64_t);
+  }
+
+  // we touched this
+  for (int j = 0; j < this->size_; j++)
+  {
+    
+    for (int i = 0; i < this->columns_.size(); i++)
+    {
+      auto &c(this->columns_[i]);
+      std::cout << c[j] << std::endl;
+    }
   }
 }
 
 // Constructor that loads relation_ from disk
-Relation::Relation(const char *file_name) : owns_memory_(false), size_(0) {
+Relation::Relation(const char *file_name) : owns_memory_(false), size_(0)
+{
   loadRelation(file_name);
 }
 
 // Destructor
-Relation::~Relation() {
-  if (owns_memory_) {
+Relation::~Relation()
+{
+  if (owns_memory_)
+  {
     for (auto c : columns_)
       delete[] c;
   }
 }
-
